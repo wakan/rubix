a2830ef224ece2ef880f5bc65de66222aa84074c
WaKaN <wakan_powa_s01@hotmail.fr>
2018-07-30 22:32:29 +0200

    init 3x3 with current state + correct swipe dynamique size of tab

--- ConsoleApp1/Helpers/ArrayHelpers.cs
+++ ConsoleApp1/Helpers/ArrayHelpers.cs
@@ -5,35 +5,14 @@ namespace ConsoleApp1.Helpers
 {
     public static class ArrayHelpers
     {
-        public class EqualityComparer<T> : IEqualityComparer<T>
-        {
-            public EqualityComparer(Func<T, T, bool> cmp)
-            {
-                this.cmp = cmp;
-            }
-            public bool Equals(T x, T y)
-            {
-                return cmp(x, y);
-            }
-
-            public int GetHashCode(T obj)
-            {
-                return obj.GetHashCode();
-            }
-
-            public Func<T, T, bool> cmp { get; set; }
-        }
-
         public static int[] SwipeTab(int[] te, int[] tt)
         {
-            int[] tr = new int[8];
+            int[] tr = new int[te.Length];
             for (int i = 0; i < te.Length; i++)
             {
                 tr[i] = tt[te[i]];
             }
             return tr;
         }
-
-
     }
 }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,12 +5,11 @@ namespace ConsoleApp1
 {
     class Program
     {
-        
-
         static void Main(string[] args)
         {
-            var init = new int[] { 2,1,0,3,4,5,6,7 };
-            var solver = new Solver(new C2x2ParamSolver(), init);
+            //var init = new int[] { 2,1,0,3,4,5,6,7 };
+            var init = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 26, 21, 22, 23, 19, 25, 24 };
+            var solver = new Solver(new C3x3ParamSolver(), init);
             var res = solver.Solve().Result;
             var strHumain = string.Join(Environment.NewLine, res.Select(r=> string.Join(string.Empty, r)));
             Console.WriteLine(strHumain);
@@ -18,8 +17,5 @@ namespace ConsoleApp1
             Console.WriteLine("Hello World!");
             Console.ReadLine();
         }
-
-       
     }
-  
 }
