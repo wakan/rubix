423638ea2b9c7af0103d2c0fe2f51fadb4942d04
WaKaN <wakan_powa_s01@hotmail.fr>
2018-07-31 21:45:41 +0200

    add log start

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine($"Start {DateTime.Now}");
             //var init = new int[] { 2,1,0,3,4,5,6,7 };
             var init = new int[] { 0, 23, 6, 11, 4, 9, 18, 17, 20, 19, 10, 9, 12, 13, 14, 15, 16, 21, 8, 5, 24, 7, 22, 25, 26, 3, 2 };
             var solver = (Solver)new SolverFirstResultLargeur(new C3x3ParamSolver(), init);
