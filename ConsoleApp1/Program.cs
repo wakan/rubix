eacbb65a5a60ece1c1ce493a2a0af878dce71c50
924421 <wakan_powa_s01@hotmail.fr>
2018-07-31 16:51:36 +0200

    Rendre generic les solvers

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,7 +9,7 @@ namespace ConsoleApp1
         {
             //var init = new int[] { 2,1,0,3,4,5,6,7 };
             var init = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 26, 21, 22, 23, 19, 25, 24 };
-            var solver = new Solver(new C3x3ParamSolver(), init);
+            var solver = (Solver)new SolverVertical(new C3x3ParamSolver(), init);
             var res = solver.Solve().Result;
             var formatter = new SolutionsConsoleFormater(res);
             var strHumain = formatter.Format();
--- ConsoleApp1/Solver.cs
+++ ConsoleApp1/Solver.cs
@@ -1,73 +1,12 @@
-﻿using ConsoleApp1.Helpers;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    public class Solver
+    public interface Solver
     {
-        readonly ParamSolver _ParamSolver;
-        readonly int[] _PositionInitials;
-        public Solver(ParamSolver paramSolver, int[] positionInitials)
-        {
-            _ParamSolver = paramSolver;
-            _PositionInitials = positionInitials;
-        }
-        int countNeedOptim = 0;
-        public Task<List<List<Move>>> Solve()
-        {
-            Stopwatch sw = new Stopwatch();
-            sw.Start();
-            var mvts = new Stack<Move>(_ParamSolver.DeepthMaxSearch);
-            var res = search(0, _PositionInitials, mvts);
-            sw.Stop();
-            var time = sw.ElapsedMilliseconds;
-            return Task.FromResult(res);
-        }
-
-        string elementInHash;
-        HashSet<string> listResultPasse = new HashSet<string>();
-
-        List<List<Move>> search(int deepth, int[] tc, Stack<Move> mvts)
-        {
-            if (deepth >= _ParamSolver.DeepthMaxSearch)
-            {
-                return new List<List<Move>>();
-            }
-            if (Enumerable.SequenceEqual(tc, _ParamSolver.Tr))
-            {
-                return new List<List<Move>> { getResultFromListMvtsCurrent(mvts) };
-            }
-            elementInHash = string.Join(string.Empty, tc);
-            if (listResultPasse.Contains(elementInHash))
-            {
-                ++countNeedOptim;
-                return new List<List<Move>>();
-            }
-
-            var retour = new List<List<Move>>();
-
-            foreach (var move in _ParamSolver.Tt)
-            {
-                if (_ParamSolver.MustSkipForOptim(move.Key, mvts))
-                    continue;
-                listResultPasse.Add(elementInHash);
-                var ttc = move.Value;
-                var trswipe = ArrayHelpers.SwipeTab(tc, ttc);
-                mvts.Push(move.Key);
-                var ret = search(deepth + 1, trswipe, mvts);
-                retour.AddRange(ret);
-                mvts.Pop();
-            }
-            return retour;
-        }
-
-        List<Move> getResultFromListMvtsCurrent(Stack<Move> mvts)
-        {
-            return mvts.Reverse().ToList();
-        }
-
+        ParamSolver Param { get; set; }
+        int[] PositionInitial { get; set; }
+        Task<List<List<Move>>> Solve();
     }
 }
